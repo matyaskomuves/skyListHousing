c0f19966c6b93fa7f74b1e8918a67031a98d9508
matyaskomuves <mkomuves714@gmail.com>
2025-02-17 18:15:10 +0100

    Contact Page // ReactiveFormsModule

--- skyListHousing/src/app/contact/contact.component.ts
+++ skyListHousing/src/app/contact/contact.component.ts
@@ -1,11 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required, Validators.minLength(25)]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.contactForm.valid) {
+      console.log('Form Submitted', this.contactForm.value);
+      alert('Your message has been sent!');
+      this.contactForm.reset();
+    }
+  }
 
 }
