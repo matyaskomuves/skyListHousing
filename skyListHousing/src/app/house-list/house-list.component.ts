2c69b008238d53252b849171862bd5f9dd55df5e
matyaskomuves <mkomuves714@gmail.com>
2025-02-21 16:33:15 +0100

    CSS/animations updates

--- skyListHousing/src/app/house-list/house-list.component.ts
+++ skyListHousing/src/app/house-list/house-list.component.ts
@@ -13,10 +13,10 @@ import { trigger, transition, style, animate } from '@angular/animations';
     trigger('fadeInOut', [
       transition(':enter', [
         style({ opacity: 0, transform: 'translateY(-10px)' }),
-        animate('300ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
+        animate('500ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
       ]),
       transition(':leave', [
-        animate('200ms ease-in', style({ opacity: 0, transform: 'translateY(-10px)' }))
+        animate('300ms ease-in', style({ opacity: 0, transform: 'translateY(-10px)' }))
       ])
     ])
   ]
