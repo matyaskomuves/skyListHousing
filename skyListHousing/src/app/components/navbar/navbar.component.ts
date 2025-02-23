2b3d9517371c4b67936f5efbeec6fb90d1921fee
matyaskomuves <mkomuves714@gmail.com>
2025-02-23 15:24:48 +0100

    Optimized navbar, CSS changes

--- skyListHousing/src/app/components/navbar/navbar.component.ts
+++ skyListHousing/src/app/components/navbar/navbar.component.ts
@@ -2,19 +2,44 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ViewChild } from '@angular/core';
 import { ElementRef } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-navbar',
   imports: [CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(-10px)' }),
+        animate('500ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
+      ]),
+      transition(':leave', [
+        animate('300ms ease-in', style({ opacity: 0, transform: 'translateY(-10px)' }))
+      ])
+    ])
+  ]
 })
 export class NavbarComponent {
 
   isMenuOpen = false;
+  isDesktop = typeof window !== 'undefined' && window.innerWidth > 768;
+
+  constructor() {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('resize', () => this.checkScreenSize());
+    }
+  }
 
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
   }
 
+  checkScreenSize() {
+    if (typeof window !== 'undefined') {
+      this.isDesktop = window.innerWidth > 768;
+    }
+
+  }
 }
