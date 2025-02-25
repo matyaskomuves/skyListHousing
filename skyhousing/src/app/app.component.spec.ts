0d209bd25aa6c83f74edc1176039585d0313743d
matyaskomuves <mkomuves714@gmail.com>
2025-02-25 19:46:46 +0100

    added Firebase

--- skyhousing/src/app/app.component.spec.ts
+++ skyhousing/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'skyListHousing' title`, () => {
+  it(`should have the 'skyhousing' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('skyListHousing');
+    expect(app.title).toEqual('skyhousing');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, skyListHousing');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, skyhousing');
   });
 });
---
+++ skyhousing/src/main.ts
@@ -0,0 +1,19 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { initializeAppCheck, ReCaptchaEnterpriseProvider, provideAppCheck } from '@angular/fire/app-check';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideAnimations(), provideFirebaseApp(() => initializeApp({ projectId: "projectskyhousing", appId: "1:607036298333:web:edd72454a0ca59388869b4", storageBucket: "projectskyhousing.firebasestorage.app", apiKey: "AIzaSyCSeIw04-rCs4gPoL0Xl-lFeGhxpRC0-7A", authDomain: "projectskyhousing.firebaseapp.com", messagingSenderId: "607036298333" })), provideAppCheck(() => {
+  // TODO get a reCAPTCHA Enterprise here https://console.cloud.google.com/security/recaptcha?project=_
+  const provider = new ReCaptchaEnterpriseProvider(/* reCAPTCHA Enterprise site key */);
+  return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
+})
+  ]
+})
+  .catch(err => console.error(err));
